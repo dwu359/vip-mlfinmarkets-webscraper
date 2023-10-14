256af61fe1ede86c3c37d764bd56c694f1742643
Daniel Wu <dwu359@gatech.edu>
2023-10-14 13:09:47 -0400

    set node runtime

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,6 +1,9 @@
 import { StackContext, Api, EventBus } from "sst/constructs";
 
 export function API({ stack }: StackContext) {
+  stack.setDefaultFunctionProps({
+    runtime: "nodejs18.x",
+  });
   const api = new Api(stack, "api", {
     routes: {
       "GET /": "packages/functions/src/scrape.handler",
